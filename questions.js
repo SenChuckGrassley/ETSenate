15b69ab7198c9d91d03da04c8f22450555142be5
SenChuckGrassley <82342794+SenChuckGrassley@users.noreply.github.com>
2021-04-11 18:07:05 -0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,11 +1,11 @@
 questions = [
     {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "question": "Should we Defund the Police?",
         "effect": {
-            "econ": 10,
-            "dipl": 0,
-            "govt": -5,
-            "scty": 0
+            "Republicans": -10,
+            "Working Families Party": 10,
+            "New Deal Party": 5,
+            "Democrats": -5
         }
     },
     {
